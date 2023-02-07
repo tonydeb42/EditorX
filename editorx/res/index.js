805e035ea39a48f00e908a7eb7e2c1d00aa52d91
tonydeb42 <122314641+tonydeb42@users.noreply.github.com>
2023-02-08 00:00:55 +0530

    Delete editorx directory

--- editorx/res/index.js
+++
@@ -1,9 +0,0 @@
-let html=document.getElementById("html");
-let css=document.getElementById("css");
-let js=document.getElementById("js");
-let result=document.getElementById("result");
-setInterval(run,1000);
-function run(){
-    result.contentDocument.body.innerHTML=html.value+(`<style>${css.value}</style>`);
-    result.contentWindow.eval(js.value);
-}
